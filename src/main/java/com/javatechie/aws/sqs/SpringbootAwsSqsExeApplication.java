c9032fd0c3a7ab95056b41e08603fd8c17f2ebe7
Dheeraj <dheeraj1990kk@gmail.com>
2021-08-30 20:51:10 +0530

    sqs

---
+++ src/main/java/com/javatechie/aws/sqs/SpringbootAwsSqsExeApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.aws.sqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
+
+@SpringBootApplication(exclude = {ContextStackAutoConfiguration.class})
+public class SpringbootAwsSqsExeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAwsSqsExeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/javatechie/aws/sqs/controller/AmazonSqsController.java
@@ -0,0 +1,25 @@
+package com.javatechie.aws.sqs.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AmazonSqsController {
+
+    @Autowired
+    private QueueMessagingTemplate queueMessagingTemplate;
+
+    @Value("${cloud.aws.end-point.uri}")
+    private String endpoint;
+
+    @GetMapping("/send/{message}")
+    public String sendMessageToQueue(@PathVariable String message) {
+        queueMessagingTemplate.send(endpoint, MessageBuilder.withPayload(message).build());
+        return message;
+    }
+}
